280be9d0d4e664e425d8e9e4a2f0f61e50893e4f
Collin Miller <collintmiller@gmail.com>
2019-12-02 10:20:41 -0600

    use AbortController to cancel in-flight requests

--- app/util/use-fetch.js
+++ app/util/use-fetch.js
@@ -4,35 +4,47 @@ export default function useFetch(url, fetchOptions = {}, options = {}) {
   const [inFlight, setInFlight] = useState(!(options.fetchNow === false));
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
-  const [abortController, setAbortController] = useState(new AbortController());
-
-  async function doFetch(extraFetchOptions) {
-    try {
-      // TODO: figure out how to cancel the last request while making the next
-      //abortController.abort();
-      setInFlight(true);
-      setError(null);
-      const request = window.fetch(url, {
-        ...fetchOptions,
-        ...extraFetchOptions,
-        //signal: abortController.signal
-      });
-      const response = await request;
-      const data = await response.json();
-      setData(data);
-      setInFlight(false);
-    } catch (error) {
-      //if (error.message.match(/aborted/)) return
-      setInFlight(false);
-      setError(error);
-      setData(null);
-    }
-  }
+  const [fetchTrigger, setFetchTrigger] = useState(options.fetchNow === false ? 0 : 1);
+  const [extraFetchOptions, setExtraFetchOptions] = useState({});
 
   useEffect(() => {
-    if (options.fetchNow === false) return;
-    doFetch();
-  }, [url, options.fetchNow]);
+    if (fetchTrigger === 0) return
+
+    const abortController = new AbortController();
+    let aborted = false;
+    async function executeFetch() {
+      try {
+        setInFlight(true);
+        setError(null);
+        const request = window.fetch(url, {
+          ...fetchOptions,
+          ...extraFetchOptions,
+          signal: abortController.signal
+        });
+        const response = await request;
+        const data = await response.json();
+        setData(data);
+        setInFlight(false);
+      } catch (error) {
+        setInFlight(false);
+        if (aborted === false) {
+          setError(error);
+          setData(null);
+        }
+      }
+    }
+    executeFetch();
+
+    return () => {
+      aborted = true;
+      abortController.abort();
+    }
+  }, [url, fetchTrigger])
+
+  function doFetch (extraFetchOptions={}) {
+    setExtraFetchOptions(extraFetchOptions);
+    setFetchTrigger(count => count + 1);
+  }
 
   return {data, inFlight, error, doFetch};
 }
